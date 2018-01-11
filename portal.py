6f42087d59a2c33310093dd9fff816ef4fd50d87
Vincent Carluer <vince@bitfu.io>
2018-01-11 21:57:14 +0000

    free space

--- portal.py
+++ portal.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import subprocess
 import logging
 from flask import Flask, render_template, make_response, request, send_from_directory, abort
 
@@ -7,4 +8,8 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     user = request.headers.get("X-User-Id")
-    return render_template('home.html', user=user)
+    df = subprocess.Popen(["df", "-h", "/home/plexlib"], stdout=subprocess.PIPE)
+    output = df.communicate()[0]
+    device, size, used, available, percent, mountpoint = \
+                output.split("\n")[1].split()
+    return render_template('home.html', user=user, freeSpace=available)
