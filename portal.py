cb3224def937c187c8bdc858d7672d590a96d46c
Vincent Carluer <vince@bitfu.io>
2018-01-11 21:32:05 +0000

    header

--- portal.py
+++ portal.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    user = request.headers.getlist("X-User-Id")
+    user = request.headers.get("X-User-Id")
     return render_template('home.html', user=user)
